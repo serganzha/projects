6e557cfb2999bb65f646789b1382ae122ce3105d
Sergii Ganzha <129215190+serganzha@users.noreply.github.com>
2023-05-23 16:56:31 +0100

    Delete menu.py

--- menu.py
+++
@@ -1,7 +0,0 @@
-item_1=input("Please enter what you would like to order: ")
-item_2=input("Please enter another of your favourite foods: ")
-item_3=input("One more for good luck: ")
-print("Thank you, You have ordered the following: ")
-print(item_1)
-print(item_2)
-print(item_3)
